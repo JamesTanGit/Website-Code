deaa298a038e799909fe5c90dc24504bf96cc87a
JamesTanGit <jamestanprogrammer@gmail.com>
2018-08-19 23:42:47 -0400

    added some js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function () { // document.addEventListener("DOMContentLoaded")
+	$("#navbarToggle").blur(function (event) { // document.querySelector("#navbarToggle").addEventListener("blur")
+		var screenWidth = window.innerWidth;
+		if (screenWidth < 768) {
+			$("#collapsable-nav").collapse('hide');
+		}
+	});
+});
