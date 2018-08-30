b9593c98b87fc5c94526f72acd1573ef18da3f74
JamesTanGit <jamestanprogrammer@gmail.com>
2018-08-30 19:54:10 -0400

    test

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 document.querySelector(".button-finish").addEventListener("click", function () {
 
   // Call the server to get the message
-  $ajaxUtils.sendGetRequest("/data/finish-welcome.json", function (res) {
+  // $ajaxUtils.sendGetRequest("/data/finish-welcome.json", function (res) {
+    $ajaxUtils.sendGetRequest("https://api.myjson.com/bins/xtwe4", function (res) {
     var message = res.string;
     document.querySelector("#content").innerHTML = message;
   });
